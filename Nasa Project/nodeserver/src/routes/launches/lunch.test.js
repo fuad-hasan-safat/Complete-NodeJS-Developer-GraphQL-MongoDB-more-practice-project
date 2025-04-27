9b1d202104d400c9233ecdfa5750c2653008d2a0
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2025-04-27 18:31:30 +0600

    supertest added

--- Nasa Project/nodeserver/src/routes/launches/lunch.test.js
+++ Nasa Project/nodeserver/src/routes/launches/lunch.test.js
@@ -1,15 +1,20 @@
-describe('Test GET/launches', ()=>{
-    test("It should response with 200 sucess", ()=>{
-        const response = 200;
-        expect(response).toBe(200)
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test GET/launches', () => {
+    test("It should response with 200 sucess", async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200)
     })
 })
 
-describe('Test POST/launch', ()=> {
+describe('Test POST/launch', () => {
     test('It should respond with 200 success', () => {
 
     })
 
-    test('It should catch missing required properties', ()=>{})
-    test('It should catch invalid dates',()=>{})
+    test('It should catch missing required properties', () => { })
+    test('It should catch invalid dates', () => { })
 })
