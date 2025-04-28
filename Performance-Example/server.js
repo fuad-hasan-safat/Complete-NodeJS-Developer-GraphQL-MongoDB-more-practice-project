c1806b42c8c0fbdd4b1695715752be6a8de28d63
fuad-hasan-safat <fuadsafat16@gmail.com>
2025-04-28 08:02:48 +0600

    add cluster

---
+++ Performance-Example/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cluster = require('cluster');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+function delay(duration) {
+    const startTime = Date.now();
+    while (Date.now() - startTime < duration) {
+        // Busy wait
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send(`Performance example: ${process.pid}`);
+});
+
+
+app.get('/timer', (req, res) => {
+    delay(9000);
+    res.send(`dealyed response: ${process.pid}`);
+});
+
+
+
+if (cluster.isMaster) {
+    console.log(`Master  is running`);
+    cluster.fork();
+    cluster.fork();
+} else {
+    console.log(`Worker started`);
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
