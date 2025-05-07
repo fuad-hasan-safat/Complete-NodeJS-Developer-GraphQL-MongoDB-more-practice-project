4aa5c94a925d3f39b52b95a21416280d10f96773
fuad-hasan-safat <fuadsafat16@gmail.com>
2025-05-07 08:36:01 +0600

    grapgql

--- GraphQl/server.js
+++ GraphQl/server.js
@@ -5,7 +5,9 @@ const schema = require('./schema');
 // Create an express instance serving all methods on `/graphql`
 // where the GraphQL over HTTP express request handler is
 const app = express();
-app.post('/graphql', createHandler({ schema }));
+app.post('/graphql', createHandler({ 
+    schema, // GraphQL schema
+ }));
 
 app.listen({ port: 4000 });
 console.log('GraphQL Server Listening to port 4000');
