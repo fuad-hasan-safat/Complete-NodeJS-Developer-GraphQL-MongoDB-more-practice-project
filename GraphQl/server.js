8f978e03fd31a5b3f8c697c4d61b92fa8a25c266
fuad-hasan-safat <fuadsafat16@gmail.com>
2025-05-23 08:11:57 +0600

    graph ql added

--- GraphQl/server.js
+++ GraphQl/server.js
@@ -1,13 +1,102 @@
-const express = require('express'); 
-const { createHandler } = require('graphql-http/lib/use/express');
-const schema = require('./schema');
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+// const schema = require('./schema');
 
+
+const schema = buildSchema(
+    `
+    type Query {
+        products: [Product]
+    }
+
+    type Product {
+        id: ID!
+        price: Float!
+        review: [Review]
+        description: String!
+    }
+
+    type Review {
+        rating: Int!
+        text: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
+    }
+    `
+)
 // Create an express instance serving all methods on `/graphql`
 // where the GraphQL over HTTP express request handler is
+const root = {
+    products: [
+        {
+            id: 'redshoe',
+            price: 100,
+            description: 'Good',
+            review: [{ rating: 5, text: 'Good' }]
+        },
+        {
+            id: 'blueshoe',
+            price: 200,
+            description: 'Good',
+            review: [{ rating: 4, text: 'Good' }]
+        },
+        {
+            id: 'greenshoe',
+            price: 300,
+            description: 'Good',
+            review: [{ rating: 3, text: 'Good' }]
+        },
+        {
+            id: 'whiteshoe',
+            price: 400,
+            description: 'Good',
+            review: [{ rating: 2, text: 'Good' }]
+        },
+        {
+            id: 'blackshoe',
+            price: 500,
+            description: 'Good',
+            review: [{ rating: 1, text: 'Good' }]
+        },
+    ],
+
+    orders: [
+        {
+            date: '2020-01-01',
+            subtotal: 100,
+            items: [
+                {
+                    product: {
+                        id: 'redshoe',
+                        price: 100,
+                        description: 'Good',
+                        // review: [{ rating: 5, text: 'Good' }]
+                    },
+                    quantity: 1
+                }
+            ]
+        }
+    ]
+}
+
 const app = express();
-app.post('/graphql', createHandler({ 
-    schema, // GraphQL schema
- }));
+
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema, // GraphQL schema
+    rootValue: root, // root value
+    graphiql: true,
+}));
 
 app.listen({ port: 4000 });
 console.log('GraphQL Server Listening to port 4000');
